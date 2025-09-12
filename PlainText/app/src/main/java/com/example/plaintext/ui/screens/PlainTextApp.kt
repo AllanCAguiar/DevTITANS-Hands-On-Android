6502883d081c2855f6227ffe48511c45f0fb5af0
AllanCAguiar <allan.aguiar@icomp.ufam.edu.br>
2025-09-11 23:17:11 -0400

    Revert "Created Login Screen Composable"
    This reverts commit 8343ba9428146d22a3049f5fa304cee697b9d786.

--- PlainText/app/src/main/java/com/example/plaintext/ui/screens/PlainTextApp.kt
+++ PlainText/app/src/main/java/com/example/plaintext/ui/screens/PlainTextApp.kt
@@ -1,13 +1,25 @@
 package com.example.plaintext.ui.screens
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.toRoute
 import com.example.plaintext.data.model.PasswordInfo
 import com.example.plaintext.ui.screens.editList.EditList
 import com.example.plaintext.ui.screens.hello.Hello_screen
+import com.example.plaintext.ui.screens.list.AddButton
+import com.example.plaintext.ui.screens.list.ListView
 import com.example.plaintext.ui.screens.login.Login_screen
+import com.example.plaintext.ui.screens.login.TopBarComponent
+import com.example.plaintext.ui.screens.preferences.SettingsScreen
+import com.example.plaintext.ui.viewmodel.ListViewModel
+import com.example.plaintext.ui.viewmodel.PreferencesViewModel
 import com.example.plaintext.utils.parcelableType
 import kotlin.reflect.typeOf
 
@@ -17,20 +29,28 @@ fun PlainTextApp(
 ) {
     NavHost(
         navController = appState.navController,
-        startDestination = Screen.Login,
-    ) {
-        composable<Screen.Hello> {
+        startDestination = Screen.Hello("DevTITANS"),
+    )
+    {
+        composable<Screen.Hello>{
             var args = it.toRoute<Screen.Hello>()
             Hello_screen(args)
         }
-        composable<Screen.Login> {
-            Login_screen(navigateToSettings = {}, navigateToList = {})
+        composable<Screen.Login>{
+            Login_screen(
+                navigateToSettings = {},
+                navigateToList = {}
+            )
         }
         composable<Screen.EditList>(
             typeMap = mapOf(typeOf<PasswordInfo>() to parcelableType<PasswordInfo>())
         ) {
             val args = it.toRoute<Screen.EditList>()
-            EditList(args, navigateBack = {}, savePassword = { password -> Unit })
+            EditList(
+                args,
+                navigateBack = {},
+                savePassword = { password -> Unit }
+            )
         }
     }
 }
