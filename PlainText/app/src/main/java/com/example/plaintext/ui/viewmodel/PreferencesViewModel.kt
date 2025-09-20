8cf0e76adf7c54689e5ffdecde51e04ceeecaeb8
rebecagxavier <rebecaxmello@gmail.com>
2025-09-20 01:24:32 -0400

    add preferences viewmodel and composable

--- PlainText/app/src/main/java/com/example/plaintext/ui/viewmodel/PreferencesViewModel.kt
+++ PlainText/app/src/main/java/com/example/plaintext/ui/viewmodel/PreferencesViewModel.kt
@@ -5,39 +5,54 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.createSavedStateHandle
-import androidx.lifecycle.viewmodel.initializer
-import androidx.lifecycle.viewmodel.viewModelFactory
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 data class PreferencesState(
-    var login: String,
-    var password: String,
-    var preencher: Boolean
+    val login: String = "",
+    val password: String = "",
+    val preencher: Boolean = false
 )
 
 @HiltViewModel
 class PreferencesViewModel @Inject constructor(
-    handle: SavedStateHandle,
+    private val handle: SavedStateHandle,
 ) : ViewModel() {
-    var preferencesState by mutableStateOf(PreferencesState(login = "devtitans", password = "123", preencher = true))
+
+    companion object {
+        private const val keyLogin = "login"
+        private const val keyPassword = "password"
+        private const val keyPreencher = "preencher"
+    }
+
+    var preferencesState by mutableStateOf(
+        PreferencesState(
+            login = handle[keyLogin] ?: "devtitans",
+            password = handle[keyPassword] ?: "123",
+            preencher = handle[keyPreencher] ?: true
+        )
+    )
         private set
 
     fun updateLogin(login: String) {
-
+        preferencesState = preferencesState.copy(login = login)
+        handle[keyLogin] = login
     }
 
     fun updatePassword(password: String) {
-
+        preferencesState = preferencesState.copy(password = password)
+        handle[keyPassword] = password
     }
 
     fun updatePreencher(preencher: Boolean) {
-
+        preferencesState = preferencesState.copy(preencher = preencher)
+        handle[keyPreencher] = preencher
     }
 
-    fun checkCredentials(login: String, password: String): Boolean{
+    fun checkCredentials(login: String, password: String): Boolean {
         return login == preferencesState.login && password == preferencesState.password
     }
 }
+
+
+
