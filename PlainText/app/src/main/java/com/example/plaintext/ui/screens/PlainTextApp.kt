fe89384dc76503a0c0e66bcb0ea3db630bc66ce1
Frostmourne <frostmoune27@gmail.com>
2025-09-20 23:12:45 -0400

    Created Authentication method and a Toast.

--- PlainText/app/src/main/java/com/example/plaintext/ui/screens/PlainTextApp.kt
+++ PlainText/app/src/main/java/com/example/plaintext/ui/screens/PlainTextApp.kt
@@ -2,6 +2,7 @@ package com.example.plaintext.ui.screens
 
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -18,6 +19,8 @@ import com.example.plaintext.ui.screens.preferences.SettingsScreen
 import com.example.plaintext.ui.viewmodel.LoginViewModel
 import com.example.plaintext.utils.parcelableType
 import kotlin.reflect.typeOf
+import android.widget.Toast
+import androidx.compose.ui.platform.LocalContext
 
 @Composable
 fun PlainTextApp(
@@ -26,6 +29,7 @@ fun PlainTextApp(
 ) {
     val loginViewModel: LoginViewModel = viewModel()
     val loginState by loginViewModel.loginState
+    val context = LocalContext.current
     NavHost(
         navController = appState.navController,
         startDestination = Screen.Login,
@@ -35,14 +39,26 @@ fun PlainTextApp(
             var args = it.toRoute<Screen.Hello>()
             Hello_screen(args)
         }
+        composable<Screen.List> {Text("Login bem-sucedido! Você está na tela de Lista.")}
         composable<Screen.Login> {
             Login_screen(
                 loginState = loginState,
                 onLoginChanged = loginViewModel::onLoginChange,
                 onPasswordChanged = loginViewModel::onPasswordChange,
                 onRememberMeChanged = loginViewModel::onRememberMeChange,
-                onLoginClicked = { loginViewModel.onLoginClick {} },
                 navigateToSettings = {appState.navigateToPreferences()},
+                onLoginClicked = {
+                    loginViewModel.onLoginClick(
+                        navigateToList = {
+                            appState.navController.navigate(Screen.List) {
+                                popUpTo<Screen.Login> { inclusive = true }
+                            }
+                        },
+                        onLoginError = {
+                            Toast.makeText(context, "Login/Senha inválidos!", Toast.LENGTH_SHORT).show()
+                        }
+                    )
+                },
             )
         }
         composable<Screen.EditList>(
